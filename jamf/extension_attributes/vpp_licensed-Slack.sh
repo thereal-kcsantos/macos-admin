c2a0d5aa88592d9f32ca4acd8274cb69d49de3bd
KC Santos <46388876+thereal-kcsantos@users.noreply.github.com>
2024-01-15 10:00:30 -0800

    Update vpp_licensed-Slack.sh

--- jamf/extension_attributes/vpp_licensed-Slack.sh
+++ jamf/extension_attributes/vpp_licensed-Slack.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+## About ##
+# 
+# A macOS script that can be used as a Jamf Extension Attribute to determine if the the installed Slack app on 
+# a Mac is VPP Licensed (downloaded/installed from the Mac App Store)
+# 
+#
 VPPtest=`mdls /Applications/ApplicationName -name kMDItemAppStoreReceiptIsVPPLicensed | cut -d = -f 2 | xargs`
 
 [[ "$VPPtest" == "1" ]] && echo "<result>VPP Licensed</result>" || echo "<result>Not VPP Licensed</result>"
